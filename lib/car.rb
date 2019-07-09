5e0cdb74c2a6e49a9869e6da6b80a94e3d8a8f80
Connor Cowan <ccowan003@comcast.net>
2019-07-09 05:44:14 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,7 +1,9 @@
 require_relative "./vehicle.rb"
 
 class Car
+  attr_accessor :wheel_size, :number
 
-
-
+  def initialize(wheel_size, number)
+    @wheel_size = wheel_size
+    @number = number
 end
