9cb2230ee0d5995263c37baf7954b7ddb7cec815
Connor Cowan <ccowan003@comcast.net>
2019-07-09 05:43:04 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,6 +2,15 @@ class Vehicle
   attr_accessor :wheel_size, :number
 
   def initialize(wheel_size, number)
+    @wheel_size = wheel_size
+    @number = number
+  end
 
+  def go
+    "vrrrrrrrooom!"         #returns a String
+  end
+
+  def fill_up_tank
+    "filling up!"
   end
 end
