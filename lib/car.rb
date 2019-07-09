7960f342564ab2bbd117d7b1ec7317299f1c52d7
Connor Cowan <ccowan003@comcast.net>
2019-07-09 05:45:29 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,6 +1,6 @@
 require_relative "./vehicle.rb"
 
-class Car < Vehicle
+class Car < Vehicle                   #Car inherits Vehicle class
   attr_accessor :wheel_size, :number
 
   def initialize(wheel_size, number)
